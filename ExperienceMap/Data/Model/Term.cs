792b6dad99a019e09d2190912f0e363f92bf9112
Patrick Lidstone <Patrick.Lidstone@mi.mun.ca>
2024-02-26 15:36:09 -0330

    added term property to return full term title

--- ExperienceMap/Data/Model/Term.cs
+++ ExperienceMap/Data/Model/Term.cs
@@ -10,4 +10,34 @@ public class Term
     public string ID { get; set; } = "defaultID";
     public TermNo TermNo { get; set;}
     public List<Course> Courses { get; set; } = [];
+
+    public string TermLabel {
+        get {
+            switch (TermNo)
+            {
+                case TermNo.T1:
+                    return "Term 1";
+                case TermNo.T2:
+                    return "Term 2";
+                case TermNo.TS1:
+                    return "Tech Session 1";
+                case TermNo.T3:
+                    return "Term 3";
+                case TermNo.T4:
+                    return "Term 4";
+                case TermNo.TS2:
+                    return "Tech Session 2";
+                case TermNo.T5:
+                    return "Term 5";
+                case TermNo.T6:
+                    return "Term 6";
+                case TermNo.T7:
+                    return "Term 7";
+                case TermNo.TS3:
+                    return "Tech Session 3";
+                default:
+                    return "big problems!";
+            }
+        }
+    }
 }
