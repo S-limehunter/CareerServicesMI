299391fb74c385654d6de0b88790a81dccf77df0
sgtpepp-r <oliviaclaire_2002@hotmail.com>
2024-02-28 10:28:03 -0330

    Co-authored-by: S-limehunter <S-limehunter@users.noreply.github.com>

--- ExperienceMap/Data/ControllerJson.cs
+++ ExperienceMap/Data/ControllerJson.cs
@@ -14,13 +14,28 @@ public class ControllerJson(DbContext db)
                 string CourseName = "";
                 string OutcomeString = "";
                 string[] Outcomes = [];
+                bool flag = false;
 
                 string currentLine = "";
                 do {
                     currentLine = file.ReadLine(); 
                     if (currentLine.Contains("MAJOR TOPICS")) {
+                        string dud = file.ReadLine();
+                        CourseName += file.ReadLine();
+                        CourseName += file.ReadLine();
                     }  
+
+                    if (currentLine.Contains("COURSE OBJECTIVES")) {
+                        flag = true;
+                    }
+                    if (!String.IsNullOrWhiteSpace(currentLine)) {
+                        if (Char.IsDigit(currentLine[0]) && (currentLine[1] == ')' || (currentLine[1] == '.' && flag))){
+                            OutcomeString += currentLine + '*';
+                        }
+                    }
+                    Console.WriteLine(OutcomeString);
                 } while (currentLine != null);
+                //Outcomes = OutcomeString.Split('*'); 
             } 
         } catch (NotSupportedException) {
             Console.WriteLine("File format not supprted.");
