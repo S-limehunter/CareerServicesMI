0f0ccff182eee4c1f89dc6c349c7ea9dbbae3a9e
sgtpepp-r <oliviaclaire_2002@hotmail.com>
2024-03-12 16:52:58 -0230

    refactor
    Co-authored-by: S-limehunter <S-limehunter@users.noreply.github.com>

--- ExperienceMap/Data/InputData/TextToCourse.cs
+++ ExperienceMap/Data/InputData/TextToCourse.cs
@@ -5,242 +5,248 @@ namespace ExperienceMap.Data.Input;
 
 public class TextToCourse
 {
-    //private static readonly string JsonPath = Path.Combine(Directory.GetCurrentDirectory(), "ExampleData.json");
-
-    public static void ParseCourseText(string path, CourseContext db){
+    public static void ParseCourseText(string path, CourseContext db) {
         try {
-            using (var file = new StreamReader(path)) {
-                Console.WriteLine("Currently Parsing: ");
-                Console.WriteLine(path);
+            using var file = new StreamReader(path);
+            Console.WriteLine("Currently Parsing: ");
+            Console.WriteLine(path);
 
-                string CourseName = "";
-                string CourseTitle = "";
-                string OutcomeString = "";
-                List<string> Skills = [];
+            string CourseName = "";
+            string CourseTitle = "";
+            string OutcomeString = "";
+            List<string> Skills = [];
 
-                string? currentLine = "";
+            string? currentLine = "";
 
-                {
-                    string? dud = file.ReadLine();
-                    CourseName = file.ReadLine() ?? "NAME_ERR";
-                    CourseTitle = file.ReadLine() ?? "TITLE_ERR";
-                }
+            {
+                string? dud = file.ReadLine();
+                CourseName = file.ReadLine() ?? "NAME_ERR";
+                CourseTitle = file.ReadLine() ?? "TITLE_ERR";
+            }
 
-                do {
-                    currentLine = file.ReadLine(); 
-                    if (!String.IsNullOrWhiteSpace(currentLine) && currentLine.Length > 1) {
-                        if (char.IsDigit(currentLine[0]) && currentLine[1] == ')') {
-
-                            if (currentLine.Substring(2).Length <= 0){
-                                continue;
-                            }
-
-                            OutcomeString += currentLine.Remove(0, 5);
-                            string s = OutcomeString.Substring(0, 1).ToLower() + OutcomeString.Substring(1);
-                            OutcomeString = s;
-
-                            OutcomeString = "An understanding of " + OutcomeString;
-                            Skills.Add(OutcomeString);
-                        }
-
-                        if (currentLine[0] == '*') {
-
-                            OutcomeString += currentLine.Remove(0,1);
-                            Skills.Add(OutcomeString);
-                        }
-                        OutcomeString = "";
-                    }
-
-                } while (currentLine != null);
-                //Console.WriteLine(OutcomeString);
-                //Skills = OutcomeString.Split('*'); 
-                db.Courses.Add(new() {ID = CourseName, Title = CourseTitle, Outcomes = Skills});
-                db.SaveChanges();
-            } 
+            currentLine = GetOutcomes(file, ref OutcomeString, Skills);
+            db.Courses.Add(new() { ID = CourseName, Title = CourseTitle, Outcomes = Skills });
+            db.SaveChanges();
         } catch (NotSupportedException) {
             Console.WriteLine("File format not supprted.");
         }
     }
 
-    public static void ParseProgramText(string path, CourseContext db){
-        
-        try {
-            using (var file = new StreamReader(path)){
-                Console.WriteLine("Currently Parsing: ");
-                Console.WriteLine(path);
-
-                string programName = "";
-                string degreeName = "";
-                int termCounter = 0;
-                int tsCounter = 0;
-                bool flag = false;
-                Program toAdd = new();
-
-                string? currentLine = "";
-                
+    private static string? GetOutcomes(StreamReader file, ref string OutcomeString, List<string> Skills)
+    {
+        string? currentLine;
+        do
+        {
+            currentLine = file.ReadLine();
+            if (!String.IsNullOrWhiteSpace(currentLine) && currentLine.Length > 1)
+            {
+                if (char.IsDigit(currentLine[0]) && currentLine[1] == ')')
                 {
-                    string? dud = file.ReadLine();
-                    string nameLine = file.ReadLine() ?? "NAME_ERR - TITLE_ERR";
-                    string[] fullTitle = nameLine.Remove(0,5).Split(" - ");
-                    degreeName = fullTitle[0];
-                    programName = fullTitle[1];
-                }
-                
-                toAdd = new() {ID = programName};
 
-                Degree? d = db.Degrees.Find(degreeName);
-                if (d is not null){
-                    d.Programs.Add(toAdd);
-                } else {
-                    d = new() {ID = degreeName};
-                    d.Programs.Add(toAdd);
-                    db.Degrees.Add(d);
-                }
-                
-                do {
-                    currentLine = file.ReadLine();
-
-                    if (String.IsNullOrWhiteSpace(currentLine)){
+                    if (currentLine.Substring(2).Length <= 0)
+                    {
                         continue;
                     }
 
-                    if (!flag)
-                        flag = currentLine.Contains("Year 1");
+                    OutcomeString += currentLine.Remove(0, 4);
+                    Skills.Add(OutcomeString);
+                }
 
-                    if (currentLine.Substring(0, 4).ToUpper() == "TERM" || (currentLine.Contains("Technical Session") && flag)) {
-                        List<string> courseIDs = [];
+                if (currentLine[0] == '*')
+                {
 
-                        TermNo termNo = TermNo.T1;
+                    OutcomeString += currentLine.Remove(0, 1);
+                    Skills.Add(OutcomeString);
+                }
+                OutcomeString = "";
+            }
 
-                        if (currentLine.ToLower().Contains("term")){
-                            switch (termCounter)
-                            {
-                                case 0:
-                                    termNo = TermNo.T1;
-                                    break;
-                                case 1:
-                                    termNo = TermNo.T2;
-                                    break;
-                                case 2:
-                                    termNo = TermNo.T3;
-                                    break;
-                                case 3:
-                                    termNo = TermNo.T4;
-                                    break;
-                                case 4:
-                                    termNo = TermNo.T5;
-                                    break;
-                                case 5:
-                                    termNo = TermNo.T6;
-                                    break;
-                                case 6:
-                                    termNo = TermNo.T7;
-                                    break;
-                            }
+        } while (currentLine != null);
 
-                            termCounter++;
-                        } 
-                        else {
-                            switch (tsCounter)
-                            {
-                                case 0:
-                                    termNo = TermNo.TS1;
-                                    break;
-                                case 1:
-                                    termNo = TermNo.TS2;
-                                    break;
-                                case 2:
-                                    termNo = TermNo.TS3;
-                                    break;
-                            }
+        return currentLine;
+    }
 
-                            tsCounter++;
+    public static void ParseProgramText(string path, CourseContext db){
+        
+        try {
+            using var file = new StreamReader(path);
+            Console.WriteLine($"Currently Parsing:\n {path}");
+
+            Program toAdd = new();
+
+            string? currentLine = "";
+
+            GetNames(file, out string programName, out string degreeName);
+
+            toAdd = new() { ID = programName };
+
+            // instantiate degree
+            Degree? d = db.Degrees.Find(degreeName);
+            if (d is not null)
+            {
+                d.Programs.Add(toAdd);
+            }
+            else
+            {
+                d = new() { ID = degreeName };
+                d.Programs.Add(toAdd);
+                db.Degrees.Add(d);
+            }
+
+            //main loop
+            bool flag = false;
+            int termCounter = 0;
+            int tsCounter = 0;
+            do
+            {
+                currentLine = file.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(currentLine))
+                {
+                    continue;
+                }
+
+                if (!flag)
+                    flag = currentLine.Contains("Year 1");
+
+                //scan for terms
+                if (currentLine.Substring(0, 4).ToUpper() == "TERM" || (currentLine.Contains("Technical Session") && flag))
+                {
+                    TermNo termNo = TermNo.T1;
+
+                    // get term/techsesh number
+                    if (currentLine.ToLower().Contains("term"))
+                    {
+                        switch (termCounter)
+                        {
+                            case 0:
+                                termNo = TermNo.T1;
+                                break;
+                            case 1:
+                                termNo = TermNo.T2;
+                                break;
+                            case 2:
+                                termNo = TermNo.T3;
+                                break;
+                            case 3:
+                                termNo = TermNo.T4;
+                                break;
+                            case 4:
+                                termNo = TermNo.T5;
+                                break;
+                            case 5:
+                                termNo = TermNo.T6;
+                                break;
+                            case 6:
+                                termNo = TermNo.T7;
+                                break;
                         }
 
-                        toAdd.Terms.Add(new() {TermNo = termNo});
-                        //db.SaveChanges();
-
-                        ////
-                        ////
-                        bool flagtwo = false;
-                        while (!flagtwo) {
-                            currentLine = file.ReadLine();
-                            bool breakflag = false;
-
-                            if (currentLine.Length < 4){
-                                continue;
-                            }
-
-                            foreach (var letter in currentLine.Substring(0, 4).ToLower()) {
-                                breakflag = !char.IsAsciiLetterLower(letter); 
-                            }
-
-                            foreach (var number in currentLine.Substring(6, 2)) {
-                                breakflag = !char.IsDigit(number);
-                            }
-
-                            if (!breakflag){
-                                flagtwo = true;
-                            }
+                        termCounter++;
+                    }
+                    else
+                    {
+                        switch (tsCounter)
+                        {
+                            case 0:
+                                termNo = TermNo.TS1;
+                                break;
+                            case 1:
+                                termNo = TermNo.TS2;
+                                break;
+                            case 2:
+                                termNo = TermNo.TS3;
+                                break;
                         }
-                        ////
-                        ////
 
-                        /*
-                        if (currentLine.Contains(" – ")){ //THIS UNICODE CHARACTER is EVIL!!!  --> –
-                            currentLine = file.ReadLine();
-                            currentLine = file.ReadLine();
-                        } */
+                        tsCounter++;
+                    }
 
-                        
-                        while (!String.IsNullOrWhiteSpace(currentLine)) {
-                            if (currentLine == "or") {
-                                currentLine = file.ReadLine();
-                                continue;
-                            }
+                    toAdd.Terms.Add(new() { TermNo = termNo });
 
-                            if (currentLine.ToLower().StartsWith("one of:")) {
-                                currentLine = currentLine.Remove(0, 8);
-                            }
-                            
-                            courseIDs.Add(currentLine.Substring(0, currentLine.IndexOf('(')-1)); 
-                            currentLine = file.ReadLine();
-                            // idk how to relate these to the other coursea info 
+                    //go to course block
+                    bool flagtwo = false;
+                    while (!flagtwo)
+                    {
+                        currentLine = file.ReadLine();
+                        bool breakflag = false;
+
+                        if (currentLine.Length < 4)
+                        {
+                            continue;
                         }
-                        
-                        
-                        //db.SaveChanges();
-                        foreach (var course in courseIDs) {
-                            toAdd.AddCourseToTerm(db, termNo, course);
-                            //db.SaveChanges();
+
+                        foreach (var letter in currentLine.Substring(0, 4).ToLower())
+                        {
+                            breakflag = !char.IsAsciiLetterLower(letter);
+                        }
+
+                        foreach (var number in currentLine.Substring(6, 2))
+                        {
+                            breakflag = !char.IsDigit(number);
+                        }
+
+                        if (!breakflag)
+                        {
+                            flagtwo = true;
                         }
                     }
 
-                    /*if (currentLine.Contains("Technical Session")) {
-                        List<string> courseIDs = [];
-                       
-                        for (int i = 0; i < 4; i++) {
-                            currentLine = file.ReadLine();
-                        }
-                        while (!String.IsNullOrWhiteSpace(currentLine)) {
-                            // same thing 
-                            courseIDs.Add(currentLine);
-                            currentLine = file.ReadLine();
-                        }
+                    currentLine = GetCourseBlock(file, currentLine, out List<string> courseIDs);
 
-                        toAdd.Terms.Add(new() {TermNo = (TermNo)termCounter});
+                    AddCourses(db, toAdd, courseIDs, termNo);
+                }
 
-                        foreach (var course in courseIDs) {
-                            toAdd.AddCourseToTerm(db, (TermNo)termCounter, course);
-                        }
-                    }*/
-                } while(currentLine != null);
-            } 
+            } while (currentLine != null);
         }
-        catch (NotSupportedException) {
+        catch (NotSupportedException) 
+        {
             Console.WriteLine("File format not supported");
         }
 
     }
-    
+
+    private static string? GetCourseBlock(StreamReader file, string? currentLine, out List<string> courseIDs)
+    {
+        List<string> courses = [];
+
+        while (!String.IsNullOrWhiteSpace(currentLine))
+        {
+            if (currentLine == "or")
+            {
+                currentLine = file.ReadLine();
+                continue;
+            }
+
+            if (currentLine.ToLower().StartsWith("one of:"))
+            {
+                currentLine = currentLine.Remove(0, 8);
+            }
+
+            courses.Add(currentLine[..(currentLine.IndexOf('(') - 1)]);
+            currentLine = file.ReadLine();
+        }
+        
+        courseIDs = courses;
+        return currentLine;
+    }
+
+    private static void AddCourses(CourseContext db, Program toAdd, List<string> courseIDs, TermNo termNo)
+    {
+        foreach (var course in courseIDs)
+        {
+            toAdd.AddCourseToTerm(db, termNo, course);
+        }
+    }
+
+    private static void GetNames(StreamReader file, out string programName, out string degreeName)
+    {
+        _ = file.ReadLine();
+
+        string nameLine = file.ReadLine() ?? throw new NullReferenceException("Unexpected Formatting in Program Doc.");
+
+        string[] fullTitle = nameLine.Remove(0, 5).Split(" - ");
+        degreeName = fullTitle[0];
+        programName = fullTitle[1];
+    }
 }
